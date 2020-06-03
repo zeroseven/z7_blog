5580c2da70e41788b3fc9d98b01f4ce392b923fe
Raphael Thanner <r.thanner@zeroseven.de>
2020-06-03 15:17:12 +0200

    Fix typo of keys in middleware configuration

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,7 +2,7 @@
 
 return [
     'frontend' => [
-        'zerseoeven/z7_bloh/redirecthandler' => [
+        'zeroseven/z7_blog/redirecthandler' => [
             'target' => \Zeroseven\Z7Blog\Middleware\RedirectHandler::class,
             'before' => [
                 'typo3/cms-frontend/shortcut-and-mountpoint-redirect'
