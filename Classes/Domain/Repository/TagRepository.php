09c3a911e17122253ca013f34b2d242fccc5653b
Raphael Thanner <r.thanner@zeroseven.de>
2020-06-09 22:43:07 +0200

    Create tag repository

---
+++ Classes/Domain/Repository/TagRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Zeroseven\Z7Blog\Domain\Repository;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Zeroseven\Z7Blog\Domain\Model\Demand;
+use Zeroseven\Z7Blog\Domain\Model\Post;
+use Zeroseven\Z7Blog\Service\RepositoryService;
+
+class TagRepository
+{
+
+    public function findByPost(Post $post): ?array
+    {
+        if ($tagList = $post->getTagList()) {
+            return GeneralUtility::trimExplode(',', $tagList, true);
+        }
+
+        return null;
+    }
+
+    public function findByPosts(array $posts): ?array
+    {
+        $tags = [];
+
+        foreach ($posts as $post) {
+            foreach ($this->findByPost($post) ?? [] as $tag) {
+                if (!in_array($tag, $tags, true)) {
+                    $tags[] = $tag;
+                }
+            }
+        }
+
+        sort($tags, SORT_STRING);
+
+        return $tags;
+    }
+
+    public function findAll(Demand $demand, bool $ignoreTagsFromDemand = null): ?array
+    {
+
+        if ($posts = RepositoryService::getPostRepository()->findAll($ignoreTagsFromDemand === true ? $demand->setTags(null) : $demand)) {
+            return $this->findByPosts($posts->toArray());
+        }
+
+        return null;
+    }
+
+}
