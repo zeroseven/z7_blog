e862625fdc556556d2fc489fde6f3aa0e10afe54
André Schließer <a.schliesser@zeroseven.de>
2020-06-17 10:10:34 +0000

    Fix typecasting to return always a string

--- Classes/Domain/Model/Topic.php
+++ Classes/Domain/Model/Topic.php
@@ -7,13 +7,24 @@ use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 class Topic extends AbstractEntity
 {
     /** @var string */
-    protected $title;
+    protected $title = '';
 
-    public function getTitle(): ?string
+    /**
+     * Returns the title
+     *
+     * @return string $title
+     */
+    public function getTitle(): string
     {
         return $this->title;
     }
 
+    /**
+     * Sets the title and returns it self
+     *
+     * @param string $title
+     * @return Topic
+     */
     public function setTitle(string $title): self
     {
         $this->title = $title;
