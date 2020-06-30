b7fb27b8bf7843c2c7b955464b2b8fd710df76d9
Raphael Thanner <r.thanner@zeroseven.de>
2020-06-30 22:58:33 +0200

    Render info of post by static function in viewHelper

--- Classes/ViewHelpers/InfoViewHelper.php
+++ Classes/ViewHelpers/InfoViewHelper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Zeroseven\Z7Blog\ViewHelpers;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use Zeroseven\Z7Blog\Service\RepositoryService;
 use Zeroseven\Z7Blog\Utility\InfoRenderUtility;
@@ -22,11 +23,10 @@ class InfoViewHelper extends AbstractViewHelper
     }
 
 
-    public function render(): string
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
     {
+        $post = ($postUid = (int)$arguments['postUid']) ? RepositoryService::getPostRepository()->findByUid($postUid) : null;
 
-        $post = ($postUid = (int)$this->arguments['postUid']) ? RepositoryService::getPostRepository()->findByUid($postUid) : null;
-
-        return GeneralUtility::makeInstance(InfoRenderUtility::class)->render($this->arguments['file'], $post);
+        return GeneralUtility::makeInstance(InfoRenderUtility::class)->render($arguments['file'], $post);
     }
 }
