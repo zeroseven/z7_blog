d7ef4e6786e76f014f547bacb16c9b5ea0e5f94a
Raphael Thanner <r.thanner@zeroseven.de>
2020-07-02 10:54:51 +0200

    Set default ordering for authors

--- Classes/Domain/Repository/AuthorRepository.php
+++ Classes/Domain/Repository/AuthorRepository.php
@@ -2,10 +2,16 @@
 
 namespace Zeroseven\Z7Blog\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class AuthorRepository extends Repository
 {
+
+    protected $defaultOrderings = [
+        'firstName' => QueryInterface::ORDER_ASCENDING,
+        'uid' => QueryInterface::ORDER_ASCENDING
+    ];
 
     public function findByUids(array $uids): array
     {
