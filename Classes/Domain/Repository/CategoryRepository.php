3330ce9f5caf5f999be2c0e504345fbed654896b
Raphael Thanner <r.thanner@zeroseven.de>
2020-07-05 20:33:00 +0200

    Set default orderings for categories and topics

--- Classes/Domain/Repository/CategoryRepository.php
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -2,10 +2,16 @@
 
 namespace Zeroseven\Z7Blog\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
 class CategoryRepository extends AbstractPageRepository
 {
+    protected $defaultOrderings = [
+        'title' => QueryInterface::ORDER_ASCENDING,
+        'uid' => QueryInterface::ORDER_ASCENDING
+    ];
+
     public function findAll(int $belowPage = null): ?QueryResultInterface
     {
         return $this->execute($belowPage);
--- Classes/Domain/Repository/TopicRepository.php
+++ Classes/Domain/Repository/TopicRepository.php
@@ -2,9 +2,13 @@
 
 namespace Zeroseven\Z7Blog\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class TopicRepository extends Repository
 {
-
+    protected $defaultOrderings = [
+        'title' => QueryInterface::ORDER_ASCENDING,
+        'uid' => QueryInterface::ORDER_ASCENDING
+    ];
 }
