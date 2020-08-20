d829be936a6fd3ba26579d74fc4554c4843ec5a1
Raphael Thanner <r.thanner@zeroseven.de>
2020-08-20 23:25:07 +0200

    Improve links in backend info header

--- Classes/Hooks/DrawHeaderHook/AbstractHeaderHook.php
+++ Classes/Hooks/DrawHeaderHook/AbstractHeaderHook.php
@@ -2,7 +2,6 @@
 
 namespace Zeroseven\Z7Blog\Hooks\DrawHeaderHook;
 
-use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
@@ -26,21 +25,9 @@ abstract class AbstractHeaderHook
     protected function createView(string $pathAndFilename, array $variables = null): StandaloneView
     {
 
-        $editLink = GeneralUtility::makeInstance(UriBuilder::class)->buildUriFromRoute(
-            'record_edit',
-            [
-                'edit[pages][' . $this->id . ']' => 'edit',
-                'noView' => 1,
-                'returnUrl' => GeneralUtility::getIndpEnv('REQUEST_URI')
-            ]
-        );
-
         $view = GeneralUtility::makeInstance(StandaloneView::class);
         $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName($pathAndFilename));
-        $view->assignMultiple(array_merge([
-            'editLink' => $editLink,
-            'state' => FlashMessage::INFO
-        ], $variables ?: []));
+        $view->assignMultiple(array_merge(['state' => FlashMessage::INFO], $variables ?: []));
 
         return $view;
     }
