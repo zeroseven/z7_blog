644f8e838330cc9de8d4cef3464ccdb71cf2c5cc
Raphael Thanner <r.thanner@zeroseven.de>
2020-08-27 14:46:40 +0200

    Update variable name in TCA configuration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,27 +2,27 @@
 
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(static function (array $cTypes) {
+call_user_func(static function (array $CTypes) {
 
-    foreach ($cTypes as $cType) {
+    foreach ($CTypes as $CType) {
 
         // Create resource identifier
-        $resourceIdentifier = 'content-' . str_replace('_', '-', $cType);
+        $resourceIdentifier = 'content-' . str_replace('_', '-', $CType);
 
         // Add some default fields to the content elements by copy configuration of "header"
-        $GLOBALS['TCA']['tt_content']['types'][$cType]['showitem'] = $GLOBALS['TCA']['tt_content']['types']['header']['showitem'];
+        $GLOBALS['TCA']['tt_content']['types'][$CType]['showitem'] = $GLOBALS['TCA']['tt_content']['types']['header']['showitem'];
 
         // Register Flexform
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:z7_blog/Configuration/FlexForms/' . $resourceIdentifier . '.xml', $cType);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:z7_blog/Configuration/FlexForms/' . $resourceIdentifier . '.xml', $CType);
 
         // Add the flexform to the content element
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'pi_flexform', $cType, 'after:header');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'pi_flexform', $CType, 'after:header');
 
         // Register plugins
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'Zeroseven.Z7Blog',
-            ucfirst(str_replace('z7blog_', '', $cType)),
-            'LLL:EXT:z7_blog/Resources/Private/Language/locallang_db.xlf:tt_content.cType.' . $cType,
+            ucfirst(str_replace('z7blog_', '', $CType)),
+            'LLL:EXT:z7_blog/Resources/Private/Language/locallang_db.xlf:tt_content.cType.' . $CType,
             $resourceIdentifier
         );
     }
