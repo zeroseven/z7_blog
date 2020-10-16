17f88743b2cc83d03e261ecaecf772f826526846
Raphael Thanner <r.thanner@zeroseven.de>
2020-10-16 09:35:13 +0200

    Configure position of middleware interface

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -13,6 +13,9 @@ return [
         ],
         'zeroseven/z7_blog/structured_data' => [
             'target' => \Zeroseven\Z7Blog\Middleware\StructuredData::class,
+            'after' => [
+                'typo3/cms-frontend/prepare-tsfe-rendering'
+            ]
         ],
     ]
 ];
