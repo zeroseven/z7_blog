6ba330342d66e2e27a518417e6fbf62f3c0387f3
Raphael Thanner <r.thanner@zeroseven.de>
2021-06-07 14:59:09 +0000

    Remove the casting of the id parameter, as a string is specified for new data records

--- Classes/Hooks/DataHandler/RefreshPageTree.php
+++ Classes/Hooks/DataHandler/RefreshPageTree.php
@@ -11,7 +11,7 @@ class RefreshPageTree
     /** @var array */
     protected $fields = ['post_top', 'post_archive', 'post_redirect_category'];
 
-    public function processDatamap_postProcessFieldArray(bool $status, string $table, int $id, array $fieldArray, DataHandler $dataHandler): void
+    public function processDatamap_postProcessFieldArray(bool $status, string $table, $id, array $fieldArray, DataHandler $dataHandler): void
     {
         if ($table === 'pages' && !empty(array_intersect($this->fields, array_keys($fieldArray)))) {
             $dataHandler->pagetreeNeedsRefresh = true;
