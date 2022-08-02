64a4016dfb8f385bc92bf2361d9bab0dadc9ec63
Raphael Thanner <r.thanner@zeroseven.de>
2022-08-02 11:14:44 +0200

    Get flash message constant from abstract class

--- Classes/Hooks/WebLayoutHeader/AbstractHeader.php
+++ Classes/Hooks/WebLayoutHeader/AbstractHeader.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Zeroseven\Z7Blog\Hooks\WebLayoutHeader;
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
@@ -28,7 +28,7 @@ abstract class AbstractHeader
     {
         $view = GeneralUtility::makeInstance(StandaloneView::class);
         $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName($pathAndFilename));
-        $view->assignMultiple(array_merge(['state' => FlashMessage::INFO], $variables ?: []));
+        $view->assignMultiple(array_merge(['state' => AbstractMessage::INFO], $variables ?: []));
 
         return $view;
     }
