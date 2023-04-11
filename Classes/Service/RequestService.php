c5a269f263f7639af05bfa1989d91874f2cc4347
Gratian Alinei <g.alinei@users.zeroseven.de>
2023-04-11 20:47:18 +0300

    Fix error in RequestService

--- Classes/Service/RequestService.php
+++ Classes/Service/RequestService.php
@@ -1,6 +1,18 @@
 <?php
-
 declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 namespace Zeroseven\Z7Blog\Service;
 
@@ -13,7 +25,7 @@ class RequestService
 
     public static function getArguments(): array
     {
-        return GlobalUtility::getRequestParameter(self::REQUEST_KEY);
+        return GlobalUtility::getRequestParameter(self::REQUEST_KEY) ?? [];
     }
 
     public static function getArgument(string $key): ?string
